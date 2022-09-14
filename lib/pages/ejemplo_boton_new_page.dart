6d98e86958699a79efe49e3d8c477d6ee0b20b29
AlumnosInf <AlumnosInf@soportetiu.local>
2022-09-14 16:27:55 -0300

    Adsds

--- lib/pages/ejemplo_boton_new_page.dart
+++ lib/pages/ejemplo_boton_new_page.dart
@@ -1,5 +1,9 @@
+import 'package:ejemplo2asdf1409/pages/new_content/new_content_page1.dart';
+import 'package:ejemplo2asdf1409/pages/new_content/new_content_page2.dart';
+import 'package:ejemplo2asdf1409/pages/new_content/new_content_page3.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class EjemploBotonNewPage extends StatefulWidget {
   EjemploBotonNewPage({Key? key}) : super(key: key);
@@ -9,29 +13,44 @@ class EjemploBotonNewPage extends StatefulWidget {
 }
 
 class _EjemploBotonNewPageState extends State<EjemploBotonNewPage> {
+  final paginas = [NcPage1(), NcPage2(), NcPage3()];
+  int paginaSel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Ejemplo 1 boton new page')),
-      body: Text('Ejemplo 1'),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(MdiIcons.accessPoint),
-          label: 'Pagina 1',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(MdiIcons.accessPoint),
-          label: 'Pagina 2',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(MdiIcons.accessPoint),
-          label: 'Pagina 3',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(MdiIcons.accessPoint),
-          label: 'Pagina 4',
-        ),
-      ]),
+      body: paginas[paginaSel],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: paginaSel,
+          onTap: (index) {
+            //print('Tap' + index.toString());
+            setState(() {
+              paginaSel = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.algolia),
+              label: 'Pagina 1',
+              backgroundColor: Color(0xFFD5C5C8),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.circleUser),
+              label: 'Pagina 2',
+              backgroundColor: Color(0xFF564256),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.hippo),
+              label: 'Pagina 3',
+              backgroundColor: Color(0xFF51E5FF),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.fish),
+              label: 'Pagina 4',
+              backgroundColor: Color(0xFF007EA7),
+            ),
+          ]),
     );
   }
 }
